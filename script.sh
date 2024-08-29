f51969f60c78c9e9d73c56bd1b4b1ce97fcd0ad7
Elvis Nipah <elvisnipah@gmail.com>
2024-08-29 20:07:04 +0000

    add azure cli function

--- script.sh
+++ script.sh
@@ -5,7 +5,6 @@
 # check if the first argument is an existing file
 check_if_file_exists() {
   if [ -f "$1" ]; then
-    echo "This is a file"
     return 0
   else
     echo "File with the path '$1' does not exist"
@@ -24,7 +23,7 @@ fi
 # checks if the second argument is also empty, and prompts
 # the user for it
 if [ -z "$2" ]; then
-  read -p "Enter the storage blob name you want to upload to: " arg2
+  read -p "Enter the blob container name you want to upload to: " arg2
 else
   arg2="$2"
 fi
@@ -48,7 +47,19 @@ fi
 # checks the return code of the last executed command
 # 0 means success, non-zero means fail, usually
 if [ $? -eq 0 ]; then
-  echo "The function succeeded."
+  az storage blob upload \
+  --account-name elvisnipahstorage \
+  --container-name "$arg2" \
+  --name "$arg1" \
+  --file "$arg1" \
+  --auth-mode login \
+  2> log.txt
+
+  if [ $? -eq 0 ]; then
+    echo "It worked"
+  else
+    echo "It did not work :<"
+  fi
 else
   echo "The function failed"
 fi
